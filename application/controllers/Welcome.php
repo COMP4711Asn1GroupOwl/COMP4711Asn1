5eb68c19e56f56e9fc84050e82c68de93ff793cc
Gisu Kim <sksmszhdzk@gmail.com>
2017-10-06 15:36:38 -0700

    switched template to lab4

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,26 +1,32 @@
 <?php
-
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application
 {
+	
+	function __construct()
+	{
+		parent::__construct();
+	}
 
 	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/
-	 * 	- or -
-	 * 		http://example.com/welcome/index
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 * Homepage for our app
 	 */
 	public function index()
 	{
-		$this->data['pagebody'] = 'welcome_message';
-		$this->render(); 
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+
+		// pass on the data to present, as the "authors" view parameter
+		$this->data['authors'] = $source;
+
+		$this->render();
+		
 	}
+
+
 
 }
