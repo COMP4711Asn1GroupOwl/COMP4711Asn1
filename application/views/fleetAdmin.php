81f96b46c1087bc2a0ef3f83a20f2a37691c1238
Ebon Chung <jay0000015chung@gmail.com>
2017-11-26 21:43:15 -0800

    Fix Update function in Fleet page

--- application/views/fleetAdmin.php
+++ application/views/fleetAdmin.php
@@ -10,8 +10,8 @@
             {fhourly}
             {zsubmit}
         </form>
-        <a href="/info/fleet/cancel"><input type="button" value="Cancel"/></a>
-        <a href="/info/fleet/delete"><input type="button" value="Delete"/></a>
+        <a href="/info/fleet"><input type="button" value="Cancel"/></a>
+        <a href="/info/fleet"><input type="button" value="Delete"/></a>
     </div>
     <br/>
 </div>
